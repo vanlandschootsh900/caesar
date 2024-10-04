c63ff7cbf55f9e5a7e16342c15fb39b2b56dc1be
vanlandschootsh900 <vanlandschootsh900@northwested.org>
2024-10-04 10:14:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+#shay vanlandschoot
+#10-4-24
+#caeser cpher
 
+
+
+alpha = 'abcdefghijklmnopqrstuvwxzy'
+
+new_message = ''
+
+message = input('enter message here: ').lower()
+key = int(input('enter key here'))
+#print(f'{message}')
+
+for character in message:
+    if character in alpha:
+        position = alpha.find(character)
+        new_position = (position + key) % 26
+        new_character = alpha[new_position]
+        new_message += new_character
+    
+    else:
+        new_message += character
+print('your new message is ' + new_message)
